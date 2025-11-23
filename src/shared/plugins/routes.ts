0bbfc37d4c3e135ef46afc939a5359a7a9f047c0
Max <tilyupomax@gmail.com>
2025-11-23 22:19:31 +0300

    refactor(*): add index file

--- src/shared/plugins/routes.ts
+++ src/shared/plugins/routes.ts
@@ -1,6 +1,6 @@
 import fp from "fastify-plugin";
 import type { FastifyPluginAsync } from "fastify";
-import { paymentRoutesPlugin } from "@/modules/payment/routes/payments";
+import { paymentRoutesPlugin } from "@/modules/payment/routes";
 
 export const routesPlugin: FastifyPluginAsync = fp(async (app) => {
   await app.register(paymentRoutesPlugin);
